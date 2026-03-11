49b48838fa7278feb31873c14c583c67e9c02573
teleportingtortoise <39284283+teleportingtortoise@users.noreply.github.com>
2026-03-10 17:25:40 -0700

    eliminated reliance on fmt in tools.go

--- tools.go
+++ tools.go
@@ -641,7 +641,7 @@ func launch(command string, terminal bool, terminate bool) {
 		}
 	}
 
-	msg := fmt.Sprintf("Executing command: %q; args: %q\n", cmd.Args[0], cmd.Args[1:])
+	msg := "Executing command: \"" + cmd.Args[0] + "\"; args: " + cmd.Args[1] + "\n"
 	log.Info(msg)
 
 	cmd.SysProcAttr = &syscall.SysProcAttr{
